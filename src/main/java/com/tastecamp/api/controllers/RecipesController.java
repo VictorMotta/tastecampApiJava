ca9ee89a1ed2b4441ff3231b241eaebbd00303b4
Victor Motta <victormottas.dev@gmail.com>
2023-10-18 21:04:02 -0300

    feat: add route delete recipe

--- src/main/java/com/tastecamp/api/controllers/RecipesController.java
+++ src/main/java/com/tastecamp/api/controllers/RecipesController.java
@@ -4,7 +4,9 @@ package com.tastecamp.api.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,6 +33,11 @@ public class RecipesController {
   @PostMapping
   public void create(@RequestBody @Valid RecipesDTO req) {
     repository.save(new Recipe(req));
+  }
+
+  @DeleteMapping("/{id}")
+  public void delete(@PathVariable Integer id) {
+    repository.deleteById(id);
   }
 
 }
