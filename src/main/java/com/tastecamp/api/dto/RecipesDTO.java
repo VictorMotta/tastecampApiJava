c8ee3d6243e8a668c462a8792732152429f053d2
Victor Motta <victormottas.dev@gmail.com>
2023-10-18 23:31:58 -0300

    fix: add validation constranints in recipeDto

--- src/main/java/com/tastecamp/api/dto/RecipesDTO.java
+++ src/main/java/com/tastecamp/api/dto/RecipesDTO.java
@@ -2,19 +2,26 @@ package com.tastecamp.api.dto;
 
 import com.tastecamp.api.models.Level;
 
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 
 
 
 public record RecipesDTO(
 
   @NotNull 
+  @Size(min = 2, max = 50)
   String name,
   @NotNull
+  @Size(min = 20, max = 150)
   String ingredients,
   @NotNull
+  @Max(500)
   String preparation,
   @NotNull
+  @Positive
   Integer time,
   @NotNull 
   Level level
