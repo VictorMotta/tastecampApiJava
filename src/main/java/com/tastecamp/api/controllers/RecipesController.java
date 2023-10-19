072dbcdefdbdb5ea239180827fb47f7df782506d
Victor Motta <victormottas.dev@gmail.com>
2023-10-18 21:10:10 -0300

    feat: add route update recipe

--- src/main/java/com/tastecamp/api/controllers/RecipesController.java
+++ src/main/java/com/tastecamp/api/controllers/RecipesController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,6 +39,18 @@ public class RecipesController {
   @DeleteMapping("/{id}")
   public void delete(@PathVariable Integer id) {
     repository.deleteById(id);
+  }
+
+  @PutMapping("/{id}")
+  public void update(@PathVariable Integer id, @RequestBody @Valid RecipesDTO req){
+    repository.findById(id).map(recipe -> {
+      recipe.setName(req.name());
+      recipe.setIngredients(req.ingredients());
+      recipe.setPreparation(req.preparation());
+      recipe.setTime(req.time());
+      recipe.setLevel(req.level().getDescription());
+      return repository.save(recipe);
+    });
   }
 
 }
